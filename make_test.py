bcd033cbb15b10933724ba9f4215748850d17fe2
David Bass <davidbass256@gmail.com>
2022-10-24 22:42:39 -0400

    added option to not randomize answer choices

--- make_test.py
+++ make_test.py
@@ -1,9 +1,9 @@
 # David Bass
-# Last updated 21 September 2022
+# Last updated 24 October 2022
 
 # TODO
 # Add command line arguments for specifying answer choice randomization and
-# question point values
+# question point values.
 # Add compatibility with alternate input formats, like noting correct answer
 # choices by placing them first instead of placing an asterisk before them.
 
@@ -11,45 +11,59 @@ import sys
 from string import ascii_lowercase
 
 # Handle command line arguments
+if len(sys.argv) < 3:
+    raise "Two command line arguments (input file and output file) required."
 input = sys.argv[1]
 output = sys.argv[2]
 
-# Setup
-questions = {}
+# Read input file into "lines"
 with open(input, "r") as f:
     lines = f.readlines()
 
-# Read input file
+# Read through input file
+questions = {}
 reading_question = True
 for line in lines:
+    # Remove whitespace from edges of line being read
     line = line.strip()
 
-    # Skip blank lines
+    # If line is blank, skip it and prepare to read in the next question
     if not line.strip():
         reading_question = True
         continue
-    # Add new question value
+
+    # Add new question key to "questions" dictionary
     if reading_question:
-        question = line.strip()
+        question = line
         questions[question] = []
         reading_question = False
-    # Add new answer choice as value to question key
+    # Add new answer choice as value to question key in "questions" dictionary
     else:
-        questions[question].append(line.strip())
+        questions[question].append(line)
 
-# Write output file
-num_of_questions = len(questions)
+# Write to output file
 with open(output, "w") as f:
     # Iterate through questions
     for i, question in enumerate(questions.keys()):
-        f.write(str(i + 1) + ". (1 point)\n" + question + "\n")
+        if question[0] == "=":
+            f.write(str(i + 1) + ". (1 point)\n" + question[1:] + "\n")
+            randomize_or_not = ""
+        else:
+            f.write(str(i + 1) + ". (1 point)\n" + question + "\n")
+            randomize_or_not = "#randomize"
 
-        num_of_answers = len(questions[question])
-        # Iterate through answer choices
+        # Write question to output file
+
+        # Write answer choices to output file
         for j, answer in enumerate(questions[question]):
             if answer[0] == "*":
                 f.write("*" + ascii_lowercase[j] + ". " + answer[1:] + "\n")
             else:
                 f.write(ascii_lowercase[j] + ". " + answer + "\n")
 
-        f.write("#randomize\n\n")
+        # Write keyword for randomizing answer choices, if first character of
+        # question is not "=".
+        f.write(randomize_or_not + "\n\n")
+
+
+
